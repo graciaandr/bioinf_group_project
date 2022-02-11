eca38a4eda14656008d43ee7bcaa60d27ae66ef6
graciaandr <61698737+graciaandr@users.noreply.github.com>
2022-02-11 01:39:19 +0000

    corrected genotype & allele frequencies for GBR

--- calculate_frequencies.R
+++ calculate_frequencies.R
@@ -1,5 +1,5 @@
-file = 'C:/Users/andri/Documents/Uni London/QMUL/SemesterB/BIO727P/Coding/22_GBR_renamed_genotype.csv'
-df <- read.csv(file, header = T)
+file = 'C:/Users/andri/Documents/Uni London/QMUL/SemesterB/BIO727P/Coding/22_GBR_finalGT.csv'
+# df <- read.csv(file, header = T)
 head(df)
 
 library(dplyr)
@@ -9,15 +9,16 @@ library(genetics)
 
 
 df1 <- df %>% dplyr::select(-PK_ID, -CHROM, -POS, -REF, -ALT) 
-df_test <- df1 #[1:50,]
+df_test <- df1 # [1:1000,]
 afs_ref = c()
 afs_alt = c()
 gt_00 = c()
-gt_01 = c()
-gt_10 = c()
+gt_01_10 = c()
+gt_11 = c()
 
 n = (nrow(df_test))
-# n =20
+start_time <- Sys.time()
+
 for (i in (1:n)) {
   t = as.character(as.vector(df_test[i,]))
   print(i)
@@ -32,21 +33,23 @@ for (i in (1:n)) {
   if ((dim(gt_g1)[2]) >1) gt_g1 = t(gt_g1)
 
   final_gt_g1_00 <- as.numeric(as.vector(gt_g1[2,]))[1]
-  final_gt_g1_01 <- as.numeric(as.vector(gt_g1[2,]))[2]
-  final_gt_g1_10 <- as.numeric(as.vector(gt_g1[2,]))[3]
+  final_gt_g1_01_10 <- as.numeric(as.vector(gt_g1[2,]))[2]
+  final_gt_g1_11 <- as.numeric(as.vector(gt_g1[2,]))[3]
   
   
   afs_alt <- c(afs_alt, final_af_g1_alt)
   afs_ref <- c(afs_ref, final_af_g1_ref)
   gt_00 <- c(gt_00, final_gt_g1_00)
-  gt_01 <- c(gt_01, final_gt_g1_01)
-  gt_10 <- c(gt_10, final_gt_g1_10)
+  gt_01_10 <- c(gt_01_10, final_gt_g1_01_10)
+  gt_11 <- c(gt_11, final_gt_g1_11)
   
 }
 
+end_time <- Sys.time()
+print(end_time - start_time)
 
-d = data.frame(AF_ref = afs_ref, AF_alt = afs_alt, GT00 = gt_00, GT01 = gt_01, GT10 = gt_10)
-d[is.na(d)] <- 0
+dd = data.frame(AF_ref = afs_ref, AF_alt = afs_alt, GT00 = gt_00, GT0110 = gt_01_10, GT11 = gt_11)
+dd[is.na(dd)] <- 0
 
-
-write.table(d, file = "frequencies.csv", sep = ";", col.names = TRUE, row.names = FALSE)
+View(dd) 
+write.table(dd, file = "corrected_frequencies.csv", sep = ";", col.names = TRUE, row.names = FALSE)
