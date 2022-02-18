b5617a4eb72d25805d188ef7284ed84d773aadda
Celine Lie <98458812+celinelie@users.noreply.github.com>
2022-02-18 19:33:30 +0000

    added stats select

--- webapp.py
+++ webapp.py
@@ -2,9 +2,11 @@ from cgitb import reset
 from re import search
 from flask import Flask, render_template, redirect, request, url_for, session
 from flask_bootstrap import Bootstrap
+import pandas as pd
 import functions_db_query as dbq
 
 
+# create a flask application object
 app = Flask(__name__)
 
 # flask_wft requires encryption key
@@ -22,23 +24,32 @@ def index():
 	return render_template('index.html')
 
 # return result data base on search
-@app.route('/<search_type>/<search_value>', methods = ['GET', 'POST'])
+@app.route('/search/<search_type>/<search_value>', methods = ['GET'])
 def search(search_type, search_value):
 	data = dbq.search_db(search_type, search_value)
+	# data_df = dbq.to_df(data)
+	# df_cols = data_df.columns
 	return render_template('result.html', data=data, search_type=search_type, search_value=search_value)
 
-# select population form
-@app.route('/pop_select', methods=['POST'])
-def pop_select():
+# select statistics and populations form
+@app.route('/stats_pop_select', methods=['POST'])
+def stats_pop():
 	search_type = request.form["search_type"]
 	search_value = request.form["search_value"]
-	data = dbq.search_db(search_type, search_value)
+	data = dbq.search_db(search_type, search_value) # data from db in list of tuples
 	pop_list = request.form.getlist("population")
-	# stats_list = request.form.getlist("summarystats")
-	filtered_data = dbq.select_pop(pop_list, data)
-	# filtered_data = filtered_data.to_numpy()
-	return render_template('test.html', data=filtered_data)
+	stats_list = request.form.getlist("summarystats")
+	data_df = dbq.to_df(data) # put data into dataframe for stats
 
+	if 'shannon' in stats_list:
+		shannon_df = pd.DataFrame({'pop' : pop_list}) # make df for shannon value per population
+		shannon_list = []
+		for pop in pop_list: # loop to get shannon per pop
+			shannon = dbq.calcShannonDiv(data_df, pop)
+			shannon_list.append(shannon)
+		shannon_df["shannon"] = shannon_list # input shannon values into df
+		shannon = shannon_df.to_numpy() # df to list of tuples for html
+	return render_template('stats_pop.html', data=data, shannon=shannon)
 
 
 # start the web server
