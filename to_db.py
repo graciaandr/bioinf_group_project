d3f2e459066cd9c5630a3cd4483668cc6845431f
Celine <98458812+celinelie@users.noreply.github.com>
2022-02-28 21:27:54 +0000

    comments and tidy up

--- to_db.py
+++ to_db.py
@@ -2,8 +2,58 @@ import allel as sc
 import pandas as pd
 import numpy as np
 from pyparsing import dbl_quoted_string
-import functions_db_query as dbq
 import csv, sqlite3
+
+
+################### functions ###################
+#################################################
+
+# filter out multi-allelic
+def single_alt(df):
+    df = df[df['ALT_2'].isna()] #extract rows with 1 ALT
+    df.drop(['ALT_2', 'ALT_3'], axis=1, inplace=True)
+    df.rename(columns={"ALT_1" : "ALT"}, inplace=True)
+    return df
+
+# filter out irrelevant gene IDs
+def gene_id_refinement(df):
+    # take out those Gene IDs of the sorts "ENS\d+-ENS\d+""
+    pat = r'(\w+)\-(\w+)'
+    df2 = df[df.GENEID.str.contains(pat) == False]
+    return df2
+    
+# extract SNPs and biallelic
+def snps_only(data):
+    data2 = data['ALT'].str.split(expand=True).apply(lambda x: x.str.len()) <= 1
+    data3 = data[data2.any(axis=1)]
+    data4 = data3['REF'].str.split(expand=True).apply(lambda x: x.str.len()) <= 1
+    data5 = data3[data4.any(axis=1)]
+    return data5
+
+# change genotype symbols to alphabet
+def gt_to_alp(data, start_col, end_col):
+    for pos in range(start_col, end_col):
+        data.iloc[:,pos] = np.where(data.iloc[:,pos] == '0|1', data['REF'] + data['ALT'], data.iloc[:,pos])
+        data.iloc[:,pos] = np.where(data.iloc[:,pos] == '1|0', data['ALT'] + data['REF'], data.iloc[:,pos])
+        data.iloc[:,pos] = np.where(data.iloc[:,pos] == '1|1', data['ALT'] + data['ALT'], data.iloc[:,pos])
+        data.iloc[:,pos] = np.where(data.iloc[:,pos] == '0|0', data['REF'] + data['REF'], data.iloc[:,pos])
+    return data
+
+# insert values into table in db
+def insert_table(csv_file, database_name, table_name):
+    with sqlite3.connect(database_name) as con:
+        cursor = con.cursor()
+        with open (csv_file, 'r') as i:
+            reader = csv.reader(i)
+            columns = next(reader) 
+            query = "INSERT INTO " + table_name + " ({0}) VALUES ({1})"
+            query = query.format(','.join(columns), ','.join('?' * len(columns)))
+            for data in reader:
+                cursor.execute(query, data)
+            con.commit()
+
+#################################################
+#################################################
 
 ## population sample data
 # store population info in csv file 
@@ -31,12 +81,12 @@ fkey = genegt1['FK_ID']
 genegt1.drop(labels=['FK_ID'], axis=1,inplace = True)
 genegt1.insert(0, 'FK_ID', fkey)
 # SNPs only
-genegt2 = dbq.snps_only(genegt1)
-# # remove unnecessary columns
+genegt2 = snps_only(genegt1)
+# remove unnecessary columns
 genegt3 = genegt2.drop(['CHROM', 'FILTER', 'ALLELE', 'EFFECT'], axis=1, inplace=True)
 genegt3.to_csv('22_gene_data.csv',sep = ',', index = False)
 # remove irrelevant vant Gene ID rows
-df1 = dbq.gene_id_refinement(genegt3)
+df1 = gene_id_refinement(genegt3)
 df1.to_csv("filtered_22_gene_data.csv", index = False, header = True) # save as extra df
 # df1 = pd.read_csv('filtered_22_gene_data.csv', sep=',')
 df1['FK_ID']= df1['FK_ID'].str.replace(r'^chr22', '22') # replace chr22:XXX:XXX with 22:XXX:XXX
@@ -56,21 +106,21 @@ gt2 = gt2.iloc[: ,9:]
 gt3 = pd.concat([gt1, gt2], axis=1)
 ### refine GT table
 # filter out multi-allelic
-gt3 = dbq.single_alt(gt3)
+gt3 = single_alt(gt3)
 # SNPs only
-gt4 = dbq.snps_only(gt3)
+gt4 = snps_only(gt3)
 # rearrange column
 id = gt4['ID']
 gt4.drop(labels=['ID'], axis=1,inplace = True)
 gt4.insert(0, 'PK_ID', id)
 # change genotype symbols to alphabet
-gt5 = dbq.gt_to_alp(gt4, 5, 102)
+gt5 = gt_to_alp(gt4, 5, 102)
 gt5 = gt5.drop(columns =['CHROM', 'POS', 'REF', 'ALT'])
 # to csv
 gt5.to_csv('converted_gt_mexican.csv', index=False)
 
 
-# gt al freq data
+# gt allele freq data
 freq = pd.read_csv('22_GBR_gt1.csv')
 freq = freq.iloc[:,:5]
 freq2 = pd.read_csv('22_GBR_freq.csv', sep = ';')
@@ -80,7 +130,8 @@ freq2.rename(columns={"AF_ref":"GBR_AF_REF", "AF_alt":"GBR_AF_ALT",
 (pd.concat([freq, freq2], axis=1)).to_csv('22_gt_al_freq.csv', index=False)
 
 
-################
+
+################ Insert csv into sql database ################
 
 con = sqlite3.connect("chromosome22_snps.db")
 cursor = con.cursor()
@@ -93,26 +144,7 @@ cursor.execute("""CREATE TABLE snp_table (unique_SNP_ID,CHROM,POS,rsID,REF,ALT,I
                 GIH_AF_ref,GIH_AF_alt,GIH_AC_ref,GIH_AC_alt,GIH_GT00,GIH_GT0110,GIH_GT11,GENE_ALIAS,
                 Ancestral_Allele,AA_AF,Derived_Allele,DA_AF,
                 PRIMARY KEY (unique_SNP_ID));""")
-dbq.insert_table('SNP_data_derived_AF.csv', 'chromosome22_snps.db', 'snp_table')
+insert_table('SNP_data_derived_AF.csv', 'chromosome22_snps.db', 'snp_table')
 
 con.commit()
 con.close()
-
-
-# # population_sample table
-# cursor.execute("""CREATE TABLE pop_samples (Sample,Sex, Biosample_ID, Pop_code, Pop_name, 
-#                 Superpopulation_code, Superpopulation_name, Pop_elastic_ID, Data, 
-#                 PRIMARY KEY (Sample));""")
-# dbq.insert_table('population_samples.csv', 'chr22.db', 'pop_samples')
-
-# # genotype table
-# cursor.execute("""CREATE TABLE gt_al_freq (PK_ID,CHROM,POS,REF,ALT,GBR_AF_REF,GBR_AF_ALT,GBR_GT00,GBR_GT01,GBR_GT10,
-#                 PRIMARY KEY (PK_ID));""")
-# dbq.insert_table('22_gt_al_freq.csv', 'chr22.db', 'gt_al_freq')
-
-# # gene_names table
-# cursor.execute("""CREATE TABLE gene_data (FK_ID, POS, ID, REF, ALT, IMPACT, GENE, GENEID,
-#                 FOREIGN KEY (FK_ID) REFERENCES gt_al_freq(PK_ID));""")
-# dbq.insert_table('22_gene_data.csv', 'chr22.db', 'gene_data')
-
-
